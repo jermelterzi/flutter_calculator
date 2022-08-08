46c5df918574efed90e25615efc28083a88eb63c
jermelterzi <jv_ermel@hotmail.com>
2022-08-08 11:18:42 -0300

    Setting preferred orientation to portrait

--- lib/calculator_app.dart
+++ lib/calculator_app.dart
@@ -1,11 +1,14 @@
 import 'package:calculator/pages/calculator_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CalculatorApp extends StatelessWidget {
   const CalculatorApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
     return const MaterialApp(
       home: CalculatorPage(),
     );
